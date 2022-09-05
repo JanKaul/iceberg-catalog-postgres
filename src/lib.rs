7594c895c900b78af32dd29df2abfd3575676b5f
Jan <jankaul@mailbox.org>
2022-09-05 16:53:01 +0200

    update test

--- src/lib.rs
+++ src/lib.rs
@@ -12,60 +12,13 @@ mod tests {
     use std::{collections::HashMap, sync::Arc};
 
     use iceberg_rs::catalog::table_identifier::TableIdentifier;
-    use iceberg_rs::catalog::{namespace::Namespace, Catalog};
+    use iceberg_rs::catalog::Catalog;
     use iceberg_rs::model::schema::{AllType, PrimitiveType, SchemaV2, Struct, StructField};
     use iceberg_rs::object_store::memory::InMemory;
-    use iceberg_rs::object_store::path::Path;
     use iceberg_rs::object_store::ObjectStore;
 
     use super::*;
 
-    #[tokio::test]
-    async fn test_initialization() {
-        let object_store: Arc<dyn ObjectStore> = Arc::new(InMemory::new());
-        let (catalog, connection) = catalog::PostgresCatalog::connect(
-            "test_catalog",
-            "postgres://postgres:postgres@localhost:5432/iceberg_catalog",
-            Arc::clone(&object_store),
-        )
-        .await
-        .unwrap();
-        tokio::spawn(async move {
-            if let Err(e) = connection.await {
-                eprintln!("connection error: {}", e);
-            }
-        });
-        let catalog = Arc::new(catalog);
-        catalog.initialize(&HashMap::new()).await.unwrap();
-        assert_eq!(4, 4);
-    }
-
-    #[tokio::test]
-    async fn test_list_tables() {
-        let object_store: Arc<dyn ObjectStore> = Arc::new(InMemory::new());
-        let (catalog, connection) = catalog::PostgresCatalog::connect(
-            "test_catalog",
-            "postgres://postgres:postgres@localhost:5432/iceberg_catalog",
-            Arc::clone(&object_store),
-        )
-        .await
-        .unwrap();
-        tokio::spawn(async move {
-            if let Err(e) = connection.await {
-                eprintln!("connection error: {}", e);
-            }
-        });
-        let catalog = Arc::new(catalog);
-        Arc::clone(&catalog)
-            .initialize(&HashMap::new())
-            .await
-            .unwrap();
-        catalog
-            .list_tables(&Namespace::try_new(&vec!["test".to_string()]).unwrap())
-            .await
-            .unwrap();
-        assert_eq!(4, 4);
-    }
     #[tokio::test]
     async fn test_create_update_drop_table() {
         let object_store: Arc<dyn ObjectStore> = Arc::new(InMemory::new());
@@ -110,7 +63,7 @@ mod tests {
                 ],
             },
         };
-        let table = Arc::clone(&catalog)
+        let mut table = Arc::clone(&catalog)
             .create_table(identifier.clone(), schema)
             .await
             .unwrap();
@@ -119,19 +72,16 @@ mod tests {
             .await
             .unwrap();
         assert_eq!(exists, true);
+
         let metadata_location = table.metadata_location().to_string();
-        let next_metadata_location = "data.db/test/table1/2-2.metadata.json".to_string();
-        let from: Path = metadata_location.clone().into();
-        let to: Path = next_metadata_location.clone().into();
-        object_store.copy(&from, &to).await.unwrap();
-        let _ = Arc::clone(&catalog)
-            .update_table(
-                identifier.clone(),
-                &next_metadata_location,
-                &metadata_location,
-            )
-            .await
-            .unwrap();
+
+        let transaction = table.new_transaction();
+        transaction.commit().await.unwrap();
+
+        let new_metadata_location = table.metadata_location().to_string();
+
+        assert_ne!(metadata_location, new_metadata_location);
+
         let _ = catalog.drop_table(&identifier).await.unwrap();
         let exists = Arc::clone(&catalog)
             .table_exists(&identifier)
